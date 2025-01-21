84a4e0fff6346b2a3818831a287a9a70de5b0e68
manshikandu <manshikandu@gmail.com>
2025-01-21 23:56:15 +0545

    promise operator

--- src/index.ts
+++ src/index.ts
@@ -10,71 +10,71 @@ app.listen(3000,() => {
     console.log("server is running at 3000");
 });
 
-function is_MathsByX_Available() {
-    return false;
-}
+// function is_MathsByX_Available() {
+//     return false;
+// }
 
-function is_MathsByY_Available() {
-    return false;
-}
+// function is_MathsByY_Available() {
+//     return false;
+// }
 
-function is_ScByX_Available() {
-    return false;
-}
+// function is_ScByX_Available() {
+//     return false;
+// }
 
-function is_ScByY_Available() {
-    return false;
-}
+// function is_ScByY_Available() {
+//     return false;
+// }
 
-function resultMathsBook() {
-    return new Promise ((resolve, reject) => {
-    if(is_MathsByX_Available()){
-        resolve(true);
-    }
-    else if(is_MathsByY_Available()){
-        resolve(true);
-    }else{
-        reject(false);
-    }
-    })
-}
-function resultScienceBook():Promise<string> {
-    return new Promise ((resolve, reject) => {
-    if(is_ScByX_Available()){
-        resolve('Maths by X available');
-    }
-    else if(is_ScByY_Available()){
-        resolve('Math by Y available');
-    }else{
-        reject('Both books not available');
-    }
-    })
-}
-resultMathsBook().then((result) => {
-    console.log(result);
-    if(result){
-        resultScienceBook().then(data => {
-            console.log(data);
-        }).catch(e => {
-            console.log(e);
-        })
-    }
-})
-.catch(( error ) => {
-    console.log( 'error:',error);
-})
+// function resultMathsBook() {
+//     return new Promise ((resolve, reject) => {
+//     if(is_MathsByX_Available()){
+//         resolve(true);
+//     }
+//     else if(is_MathsByY_Available()){
+//         resolve(true);
+//     }else{
+//         reject(false);
+//     }
+//     })
+// }
+// function resultScienceBook():Promise<string> {
+//     return new Promise ((resolve, reject) => {
+//     if(is_ScByX_Available()){
+//         resolve('Maths by X available');
+//     }
+//     else if(is_ScByY_Available()){
+//         resolve('Math by Y available');
+//     }else{
+//         reject('Both books not available');
+//     }
+//     })
+// }
+// resultMathsBook().then((result) => {
+//     console.log(result);
+//     if(result){
+//         resultScienceBook().then(data => {
+//             console.log(data);
+//         }).catch(e => {
+//             console.log(e);
+//         })
+//     }
+// })
+// .catch(( error ) => {
+//     console.log( 'error:',error);
+// })
 
 
 
-async function final() {
-    try {
-        let result1 = await resultMathsBook();
-        if(result1) result1 = await resultScienceBook();
-        return result1;
-    } catch (e) {
-        return Promise.reject(e);
-    }
-}
+// async function final() {
+//     try {
+//         let result1 = await resultMathsBook();
+//         if(result1) result1 = await resultScienceBook();
+//         return result1;
+//     } catch (e) {
+//         return Promise.reject(e);
+//     }
+// }
 
 // final().then(data => {
 //     console.log('data:',data);  
@@ -83,3 +83,11 @@ async function final() {
 // })
 
 
+const object1 = { id: 1, name: 'tech'};
+const object2 = {...object1,email: 'mani@gmail.com'};
+console.log(object2);
+
+const array1 = [1,2,3];
+const array2 =  [2,3,4];
+const array3 = [...array1,...array2]
+console.log(array3);
