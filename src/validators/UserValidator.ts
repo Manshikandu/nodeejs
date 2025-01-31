b6370d268aebc354ad2c629278739a284aca640c
manshikandu <manshikandu@gmail.com>
2025-01-31 13:42:02 +0545

    Uservalidator

---
+++ src/validators/UserValidator.ts
@@ -0,0 +1,57 @@
+//import { query } from "express";
+import { query,body, ValidationChain } from "express-validator";
+import { userInfo } from "os";
+import User from "../models/User";
+export class UserValidators {
+    static signup(): ValidationChain[] {
+    return [
+           body('name','name is required').isString().notEmpty() ,
+           body('phone','Phone number is required').isString(),
+           body('email','email is required').isEmail() ,
+           body('password').isAlphanumeric()
+           .isLength({ min : 5}) 
+           .withMessage('password must be between 8-20 character'),
+           body('type','User role type is required').isString(),
+           body('status','User status is required').isString(),
+        
+        ];
+    
+    }
+    static verifyUserEmail(): ValidationChain[] {
+        return [
+               body(' verification_token','verification token  is required').isNumeric() ,
+               body('email','email is required').isEmail() ,
+              
+            ];
+        
+        }
+
+        static login(): ValidationChain[] {
+            return [
+                  
+                   query('email','email is required').isEmail() 
+                   .custom((email,{req}) => {
+                    return User.findOne({
+                        email: email
+                    }).then(user => {
+                        if(user){
+                            req.user = user;
+                            return true;
+                           }else{
+                            throw("user doesn't exist");
+                           }
+                    }).catch( e => {
+                        throw new Error(e)
+                    })
+
+                   }),
+                  
+                   query('password').isAlphanumeric()
+                   .isLength({ min : 5}) 
+                   .withMessage('password must be between 8-20 character'),
+                  
+                
+                ];
+            
+            }
+}
